f7ebcc92525814950a8b29cd7b9314bb582a2fbd
Garvey <18550110380@163.com>
2019-03-12 17:50:20 +0800

    新增九宫格，添加动画

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,9 @@
     <mt-header fixed title="测试项目"></mt-header>
 
     <!-- 中间路由router-view区域 -->
-		<router-view></router-view>
+		<transition>
+			<router-view></router-view>
+		</transition>
 
     <!-- 底部Tabbar区域 -->
 		<nav class="mui-bar mui-bar-tab">
@@ -34,6 +36,20 @@
 
 <style lang="scss" scoped>
   .app-container {
-    padding-top: 40px;
-  }
+		padding-top: 40px;
+		overflow-x: hidden;
+	}
+	.v-enter {
+		opacity: 0;
+		transform: translateX(100%);
+	}
+	.v-leave-to {
+		opacity: 0;
+		transform: translateX(-100%);
+		position: absolute;
+	}
+	.v-enter-active,
+	.v-leave-active {
+		transition: all 0.3s ease;
+	}
 </style>
--- src/router.js
+++ src/router.js
@@ -10,6 +10,7 @@ import SearchContainer from './components/tabbar/SearchContainer.vue'
 // 创建路由对象
 var router = new VueRouter({
   routes: [
+    { path: '/', redirect: '/home' },
     { path: '/home', component: HomeContainer  },
     { path: '/member', component: MemberContainer  },
     { path: '/shopcar', component: ShopcarContainer  },
